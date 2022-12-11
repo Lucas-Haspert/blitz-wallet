a618760bc9817762e38c1ca7c1fe93485489196b
Lucas-Haspert <lucashaspert95@gmail.com>
2022-12-11 02:39:19 -0300

    Implementation of Bootstrap. Creation and implementation of NavBar component. Modification of the HomeView.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </nav>
+  <router-view/>
+</template>
+  
+<script>
+  export default {
+    name: 'NavBar',
+  }
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+
+nav a {
+  font-weight: bold;
+  color: white;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>BLITZ WALLET</h1>
+    <h4>Blitz Wallet es un sistema (ficticio) para gestionar tu billetera virtual</h4>
+    <h4>Compra, vende, consulta el estado del mercado y mucho más</h4>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import NavBar from '@/components/NavBar.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
